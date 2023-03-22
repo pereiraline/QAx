b8a6e4fae2f706fdd30dc1f07c2c09e3ffc8e662
Aline Santana do Nascimento Pereira <apereira@informa.com.br>
2023-03-22 17:23:39 -0300

    renderizando itens com v-for

--- js/app.js
+++ js/app.js
@@ -1,7 +1,52 @@
+const LIST = [
+    {
+        id: 1,
+        nome: 'Mestre Yoda',
+        avatar: 'images/yoda.png'
+    },
+    {
+        id: 2,
+        nome: 'C3PO',
+        avatar: 'images/c3po.png'
+    },
+    {
+        id: 3,
+        nome: 'Chewbacca Yoda',
+        avatar: 'images/chewbacca.png'
+    },
+    {
+        id: 4,
+        nome: 'Hansolo',
+        avatar: 'images/hansolo.png'
+    },
+    {
+        id: 5,
+        nome: 'Princesa Leia',
+        avatar: 'images/leia.png'
+    },
+    {
+        id: 6,
+        nome: 'Luke Skywalker',
+        avatar: 'images/luke.png'
+    },
+    {
+        id: 7,
+        nome: 'R2D2',
+        avatar: 'images/r2d2.png'
+    },
+    {
+        id: 8,
+        nome: 'Darth Vader',
+        avatar: 'images/vader.png'
+    }
+    
+]
+
 const App = new Vue({
     el: '#app',
     data: {
         title: 'Star Wars Lego',
-        userName: 'Aline'
+        userName: 'Aline',
+        characters: LIST
     }
 })
