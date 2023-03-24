5b5e69d83d8f74064e5dabc27a0c2c6e371e48ac
Aline Santana do Nascimento Pereira <apereira@informa.com.br>
2023-03-23 22:54:12 -0300

    simulando exclusão

--- js/app.js
+++ js/app.js
@@ -54,6 +54,16 @@ const App = new Vue({
         like(userName) {
             alert(`O personagem ${userName} recebeu um like!`)
         },
+
+        remove(id) {
+            const list = this.characters
+
+            const result = list.filter(item => {
+                return item.id !== id
+            })
+
+            this.characters = result
+        }, 
         search(){
 
             if (this.searchName === '') {
